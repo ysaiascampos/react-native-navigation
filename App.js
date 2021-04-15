24ae97aa281173ab4092a2371d8aa917862e8036
ysaiascampos <ysaiascampos@gmail.com>
2021-04-14 21:02:46 -0400

    push

--- App.js
+++ App.js
@@ -1,15 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Ir a detalle"
+        onPress={()=> navigation.push('Detalle')}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const DetalleScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Soy la pantalla de detalles</Text>
+      <Button
+        title="volver"
+        onPress={()=> navigation.goBack()}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  Detalle: {
+    screen: DetalleScreen
+  }
+}, { initialRouteName: 'Home'})
+
+export default createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
   container: {
